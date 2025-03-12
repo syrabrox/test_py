a99a9c31f069269b9c677e8fc0a995f7069e983b
AsA <102066638+syrabrox@users.noreply.github.com>
2025-03-12 04:59:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+import subprocess
+
+try:
+    import flask
+except ModuleNotFoundError:
+    subprocess.run(["pip", "install", "flask"])
+
 from flask import Flask
 import random
 
@@ -8,5 +16,5 @@ def home():
     return 'Hi'
 
 if __name__ == '__main__':
-    port = random.randint(1024, 9000)
+    port = random.randint(1024, 9999) 
     app.run(host='0.0.0.0', port=port)
